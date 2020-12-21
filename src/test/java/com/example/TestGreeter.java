15e6ec1ad5dfe09009b61e215affd696a0b8695a
lohith shet <lohith@vihanafashions.co.in>
2020-12-21 07:04:43 +0530

    second

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -32,7 +32,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage11() {
+  public void greetShouldIncludeGreetingMessage11() { 
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
